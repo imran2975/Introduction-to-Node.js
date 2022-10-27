f331e95a17827573ba2a6202541e5a32eaa6b960
Imran Abubakar <imranabubakar943@gmail.com>
2022-10-27 03:21:39 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, response) => {
+    if (req.url === "/") {
+        let filePath = path.join(__dirname, 'views', "home.html")
+        fs.readFile(filePath, (err, data) => {
+            if(err){
+                console.log(err);
+            }
+            response.writeHead(200, {'Content-Type': 'text/html'})
+            response.end(data)
+        })
+    } 
+    if (req.url === "/about.html") {
+        let filePath = path.join(__dirname, 'views', "about.html")
+        fs.readFile(filePath, (err, data) => {
+            if(err){
+                console.log(err);
+            }
+            response.writeHead(200, {'Content-Type': 'text/html'})
+            response.end(data)
+        })
+    } 
+    if (req.url === "/contact.html") {
+        let filePath = path.join(__dirname, 'views', "contact.html")
+        fs.readFile(filePath, (err, data) => {
+            if(err){
+                console.log(err);
+            }
+            response.writeHead(200, {'Content-Type': 'text/html'})
+            response.end(data)
+        })
+    } else{
+        let filePath = path.join(__dirname, 'views', "404.html")
+        fs.readFile(filePath, (err, data) => {
+            if(err){
+                console.log(err);
+            }
+            response.writeHead(200, {'Content-Type': 'text/html'})
+            response.end(data)
+    })
+}
+})
+
+server.listen(3000, () => {
+    console.log('Server running at http://127.0.0.1:3000/');
+})
+
+
